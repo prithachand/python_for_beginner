e2a6d4c2abd118efa804a5e0ec248e8848015154
Your Name <europritha.chand@gmail.com>
2026-05-27 20:44:15 +0545

    intro

--- 01_day/15_f_Strings.py
+++ 01_day/15_f_Strings.py
@@ -1,4 +1,10 @@
-name = "Prithaa"
-age = 14
+# name = "Prithaa"
+# age = 14
 
-print(f"My name is {name} and I am {age} years old")
+# print(f"My name is {name} and I am {age} years old")
+
+name = "PRITHA"
+age = 14
+section = "perseus"
+
+print(f"My name is {name} and I am {age} years old. I study in 9 {section}.")
