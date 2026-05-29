f899f46b649c828e5dfd2e49768db937b0bd8ec5
Your Name <europritha.chand@gmail.com>
2026-05-29 21:23:44 +0545

    wow

--- 01_day/6_Slicing.py
+++ 01_day/6_Slicing.py
@@ -1,6 +1,9 @@
-text = "Programming"
+# text = "Programming"
 
-print(text[0:4])
-print(text[3:7])
-print(text[:5])
-print(text[5:])
+# print(text[0:4])
+# print(text[3:7])
+# print(text[:5])
+# print(text[5:])
+
+note = "Jeminispinalszm"
+print(note[0:5])
