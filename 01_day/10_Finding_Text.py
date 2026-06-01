1f70e139811ccf854368a25f60d1b70b5168b407
Your Name <europritha.chand@gmail.com>
2026-06-01 21:33:02 +0545

    new11

--- 01_day/10_Finding_Text.py
+++ 01_day/10_Finding_Text.py
@@ -2,3 +2,4 @@ text = "I love Python"
 
 print(text.find("Python"))
 print(text.find("Java"))
+print(text.find("Mingi"))
--- 01_day/9_Replacing_Words.py
+++ 01_day/9_Replacing_Words.py
@@ -3,3 +3,7 @@ text = "I like Java"
 new_text = text.replace("Java", "Python")
 
 print(new_text)
+
+
+# goooood point
+# noice
