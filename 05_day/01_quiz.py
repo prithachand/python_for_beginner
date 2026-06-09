1c97846eeba6f6dbfb2e4a5f1b5384d2c1c49209
Your Name <europritha.chand@gmail.com>
2026-06-09 20:16:29 +0545

    newwen

---
+++ 05_day/01_quiz.py
@@ -0,0 +1,13 @@
+print(f"{"Hi" * 3}")
+
+fn = "Pritha"
+ln = "Chand"
+print(fn+" "+ln)
+
+aaa = ("Hello", "4", "name is pritha")
+aac = " ".join(aaa)
+print(aac)
+
+word = "Ashbashsahtsahspassta"
+print(word.upper())
+print(word.lower())
